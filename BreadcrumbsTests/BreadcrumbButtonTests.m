5b7ffba0dced63000e18e582cf31ad7466426fe0
Konstantin.Portnov <x0000ff@gmail.com>
2016-10-02 11:35:04 -0300

    Add empty test case for `BreadcrumbButton`

---
+++ BreadcrumbsTests/BreadcrumbButtonTests.m
@@ -0,0 +1,18 @@
+//
+//  BreadcrumbButtonTests.m
+//  BreadcrumbsTests
+//
+//  Created by x0000ff on 02/10/16.
+//  Copyright © 2016 x0000ff. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "BreadcrumbButton.h"
+
+@interface BreadcrumbButtonTests : XCTestCase
+
+@end
+
+@implementation BreadcrumbButtonTests
+
+@end
