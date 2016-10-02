7191737b5dba0c92ca5dfaf07b77fb7dc2b80d93
Konstantin.Portnov <x0000ff@gmail.com>
2016-10-02 17:39:15 -0300

    Remove `CocoaPods`

--- Podfile
+++
@@ -1,9 +0,0 @@
-platform :ios, '8.0'
-
-target 'Breadcrumbs' do
-
-	target 'BreadcrumbsTests' do
-		pod 'OCMock'
-	end
-
-end
--- Pods/Target Support Files/Pods-Breadcrumbs-BreadcrumbsTests/Pods-Breadcrumbs-BreadcrumbsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Breadcrumbs_BreadcrumbsTests : NSObject
-@end
-@implementation PodsDummy_Pods_Breadcrumbs_BreadcrumbsTests
-@end
