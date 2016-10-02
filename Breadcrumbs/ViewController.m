d95f5522b4393cd8e18b971543b82ba7895de2da
Konstantin.Portnov <x0000ff@gmail.com>
2016-10-02 17:13:13 -0300

    Add more buttons

--- Breadcrumbs/ViewController.m
+++ Breadcrumbs/ViewController.m
@@ -6,19 +6,51 @@
 //  Copyright © 2016 x0000ff. All rights reserved.
 //
 
+//###########################################
 #import "ViewController.h"
 #import "BreadcrumbButton.h"
 
+//###########################################
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet BreadcrumbButton * breadcrumbButton;
+@property (weak, nonatomic) IBOutlet BreadcrumbButton * countryButton;
+@property (weak, nonatomic) IBOutlet BreadcrumbButton * cityButton;
+@property (weak, nonatomic) IBOutlet BreadcrumbButton * districtButton;
+
+@property (weak, nonatomic) IBOutlet UISwitch * countrySwitch;
+@property (weak, nonatomic) IBOutlet UISwitch * citySwitch;
+@property (weak, nonatomic) IBOutlet UISwitch * districtSwitch;
 
 @end
 
+//###########################################
 @implementation ViewController
 
+//###########################################
 - (IBAction)switchValueChanged:(UISwitch *)sender {
-    self.breadcrumbButton.enabled = sender.isOn;
+
+    BreadcrumbButton * button = [self breadcrubButtonForSwitch:sender];
+    button.enabled = sender.isOn;
 }
 
+//###########################################
+- (BreadcrumbButton *) breadcrubButtonForSwitch:(UISwitch *)switchControl {
+
+    if (switchControl == self.countrySwitch) {
+        return self.countryButton;
+    }
+
+    if (switchControl == self.citySwitch) {
+        return self.cityButton;
+    }
+
+    if (switchControl == self.districtSwitch) {
+        return self.districtButton;
+    }
+
+    return nil;
+}
+
+//###########################################
 @end
+//###########################################
